4fca6f3bc1cff06b8cb44ab70c10e3ae00db6f84
brandon marino <brandon.a.marino@gmail.com>
2016-01-21 16:54:14 -0500

    initial commit

--- app/models/spellchecker.rb
+++ app/models/spellchecker.rb
@@ -15,7 +15,7 @@ class Spellchecker
     thefile = File.open(text_file_name, "r")
     contents = thefile.read
     thefile.close
-    wordList = word(contents)
+    wordList = words(contents)
     train! (wordList)
   end
 
@@ -34,11 +34,11 @@ class Spellchecker
     #create @dictionary, an attribute of type Hash mapping words to their count in the text {word => count}. Default count should be 0 (argument of Hash constructor).
     @dictionary = Hash.new 0
     word_list.each do |word|
-      if dictionary hasKey? word
-        count = lookup word +1
-        @dictionary[item] = count
+      if @dictionary.has_key? word
+        count = lookup(word)+1
+        @dictionary[word] = count
       else
-        @dictionary[item] = 1
+        @dictionary[word] = 1
       end
     end
   end
@@ -54,13 +54,45 @@ class Spellchecker
     
     deletes    = []
     #all strings obtained by deleting a letter (each letter)
+    for i in 0..word.length-1
+        str = word.dup
+	str.slice!(i)
+        deletes.push(str)
+    end 
+    
     transposes = []
     #all strings obtained by switching two consecutive letters
+    count = word.length-2
+    if count > 0
+       (0..count).each do |i|
+	   str = word.dup
+	   str[i+1] = word[i]
+	   str[i] = word[i+1]
+ 	   transposes.push(str)
+       end
+    end
+    
     inserts = []
     # all strings obtained by inserting letters (all possible letters in all possible positions)
+    for i in 0..word.length
+    	ALPHABET.each_char do |e|
+           str = word.dup
+           str = str.insert(i,e)
+	   inserts.push(str)
+	end
+    end
+    
+    
     replaces = []
     #all strings obtained by replacing letters (all possible letters in all possible positions)
-
+    for i in 0..word.length-1
+    	ALPHABET.each_char do |e|
+           str = word.dup
+           str[i] = e
+	   replaces.push(str)
+	end
+    end
+    
     return (deletes + transposes + replaces + inserts).to_set.to_a #eliminate duplicates, then convert back to array
   end
   
@@ -68,13 +100,21 @@ class Spellchecker
   # find known (in dictionary) distance-2 edits of target word.
   def known_edits2 (word)
     # get every possible distance - 2 edit of the input word. Return those that are in the dictionary.
+    s = []
+    
+    editor1 = edits1(word)
+    editor1.each do |e|
+   	 s.concat(edits1(e))
+    end 
+
+    return known(d2)
   end
 
   #return subset of the input words (argument is an array) that are known by this dictionary
   def known(words)
-    return words.find_all {true } #find all words for which condition is true,
+    result = words.find_all {true } #find all words for which condition is true,
                                     #you need to figure out this condition
-    
+    result.empty? ? nil : result
   end
 
 
